ce6f8fbb20694d7b1be5dba3988a1133be3e5ad6
ericblam <marcusgladir@gmail.com>
2021-10-12 23:00:50 -0400

    Add dropdown handling

--- src/EntitySearch.tsx
+++ src/EntitySearch.tsx
@@ -13,9 +13,18 @@ interface IProps {
 interface IState {
     name: string;
     enabled: boolean;
+
     text: string;
     showList: boolean;
+    selected: number;
     list: Entity[]
+}
+
+const KEY = {
+    UP: "ArrowUp",
+    DOWN: "ArrowDown",
+    ENTER: "Enter"
+    // ESC?
 }
 
 class EntitySearch extends React.Component<IProps, IState> {
@@ -26,6 +35,7 @@ class EntitySearch extends React.Component<IProps, IState> {
             enabled: props.enabled,
             text: '',
             showList: false,
+            selected: -1,
             list: []
         };
     }
@@ -56,10 +66,17 @@ class EntitySearch extends React.Component<IProps, IState> {
                         placeholder={ placeholder }
                         onInput={(inp) => this.handleInput(inp)}
                         onBlur={() => this.setState({showList: false})}
-                        onFocus={() => this.setState({showList: true})}/>
+                        onFocus={() => this.setState({showList: true})}
+                        onKeyDown={(event) => this.updateSelection(event)}/>
                     <div style={{"zIndex": 5000, "position": "absolute", "width": "100%", "visibility": this.state.showList ? 'visible' : 'hidden'}}>
                         <ul>
-                            { this.state.list.map(entity => <li><a className="button is-white is-fullwidth" href={`/${lowerName}/?id=${entity.id}`}>{entity.name}</a></li>) }
+                            { this.state.list.map((entity, idx) => {
+                                const stylingClasses = `button is-fullwidth ${idx === this.state.selected ? "is-light" : "is-white"}`;
+                                return <li>
+                                    <a className={stylingClasses} href={`/${lowerName}/?id=${entity.id}`}>
+                                        {entity.name}
+                                    </a>
+                                </li>}) }
                         </ul>
                     </div>
                 </div>
@@ -69,6 +86,32 @@ class EntitySearch extends React.Component<IProps, IState> {
                 { this.state.enabled ? field : <fieldset disabled>{field}</fieldset> }
             </form>
         );
+    }
+
+    private updateSelection(event: React.KeyboardEvent) {
+        const selectedIdx = this.state.selected;
+        switch (event.key) {
+            case KEY.UP:
+                if (selectedIdx >= 0) {
+                    this.setState({selected: selectedIdx - 1});
+                }
+                break;
+            case KEY.DOWN:
+                if (selectedIdx < this.state.list.length - 1) {
+                    this.setState({selected: selectedIdx + 1});
+                }
+                break;
+            case KEY.ENTER:
+                event.preventDefault();
+                if (selectedIdx >= 0) {
+                    const selected = this.state.list[selectedIdx];
+                    const href = `/${this.props.name.toLowerCase()}/?id=${selected.id}`;
+                    window.location.href = href;
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     private updateList(search?: string) {
