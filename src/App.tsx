c0723ef91fc60b51de3ac1ed94dd432dd9b41729
ericblam <marcusgladir@gmail.com>
2021-10-24 17:39:21 -0400

    Fix routing to give 404

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
                         <Route path="/competition/:id">
                             <EntityPage />
                         </Route>
-                        <Route path="/">
+                        <Route exact path="/">
                             <Home />
                         </Route>
                         <Route path="*">
