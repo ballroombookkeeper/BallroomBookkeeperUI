c5bed6411065a8f41c643149ad523c862de3ac83
ericblam <marcusgladir@gmail.com>
2021-10-24 16:16:28 -0400

    Set up basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,47 @@ import './App.css';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import EntitySearch from './EntitySearch';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function Home() {
+    return ( 
+        <div className="columns">
+            <div className="column">
+                <EntitySearch name={"Competitor"} enabled={true} />
+            </div>
+            <div className="column">
+                <EntitySearch name={"Competition"} enabled={true} />
+            </div>
+        </div>
+    );
+}
+
+function About() {
+    return <h2>About</h2>;
+}
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <section className="section">
-                <div className="columns">
-                    <div className="column">
-                        <EntitySearch name={"Competitor"} enabled={true} />
-                    </div>
-                    <div className="column">
-                        <EntitySearch name={"Competition"} enabled={true} />
-                    </div>
-                </div>
-            </section>
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <section className="section">
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </section>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,12 +1,12 @@
+import { Link } from 'react-router-dom';
+
 import './Navbar.css';
 
 function Navbar() {
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-                <a className="navbar-item" href="/">
-                    Ballroom Bookkeeper
-                </a>
+                <Link to="/" className="navbar-item">Ballroom Bookkeeper</Link>
             
                 <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasic">
                     <span aria-hidden="true"></span>
@@ -16,9 +16,8 @@ function Navbar() {
             </div>
             <div id="navbarBasic" className="navbar-menu">
                 <div className="navbar-start">
-                    <a className="navbar-item">
-                        Home
-                    </a>
+                    <Link to="/" className="navbar-item">Home</Link>
+                    <Link to="/about" className="navbar-item">About</Link>
                 </div>
             </div>
         </nav>
